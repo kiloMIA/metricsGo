692809ed7812a0b0a2888e3891db4b7dbb4020b6
kilo <pipianino@protonmail.com>
2023-06-12 19:58:27 +0600

    pb go

--- metrics/client.py
+++ metrics/client.py
@@ -1,8 +1,7 @@
 import grpc
-import import proto.metrics.pb.metrics_pb2_grpc as met_grpc, import proto.metrics.pb.metrics_pb2 as met_pb
-logging.basicConfig(format='%(asctime)s:%(name)s : %(message)s', level=logging.DEBUG)
-log = logging.getLogger(__name__)
+import proto.metrics.pb.metrics_pb2_grpc as met_grpc, proto.metrics.pb.metrics_pb2 as met_pb
+
 socket = 'localhost:50051'
 channel = grpc.insecure_channel(socket)
 client = met_grpc.MetricsServiceStub(channel)
-log.info(f"Connected to the Server : {socket}")
+
